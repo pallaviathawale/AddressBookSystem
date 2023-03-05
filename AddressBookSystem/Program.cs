0fabdaf210d91260f0c279c4d21e40224851c7fb
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-03-05 22:33:44 +0530

    Add:UC1-CreateContact

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program");
+            Console.WriteLine("...Welcome to Address Book...");
+            AddressBooks Book = new AddressBooks();
+            Book.CreateContact();
+            Console.ReadLine();
         }
     }
-}
+    }
